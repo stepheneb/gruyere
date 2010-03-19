0f5bb018a541198a9df5b4bec166afb83521fdbd
Stephen Bannasch <stephen.bannasch@gmail.com>
2010-03-19 16:16:46 -0400

    updating post steps to work with new div structure

--- features/step_definitions/post_steps.rb
+++ features/step_definitions/post_steps.rb
@@ -4,11 +4,11 @@ end
 
 When /^I delete the (\d+)(?:st|nd|rd|th) post$/ do |pos|
   visit posts_url
-  within("table tr:nth-child(#{pos.to_i+1})") do
+  within("#posts div:nth-child(#{pos.to_i})") do
     click_link "Destroy"
   end
 end
 
 Then /^I should see the following posts:$/ do |expected_posts_table|
-  expected_posts_table.diff!(tableish('table tr', 'td,th'))
+  expected_posts_table.diff!(tableish('.expandable', '.title, .body'))
 end
