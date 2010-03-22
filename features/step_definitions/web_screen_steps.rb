e8a1b547c3292a1aaf03d6aa6ef55ed30cbc4967
Noah Paessel <knowuh@gmail.com>
2010-03-22 18:53:25 -0400

    refactoring to use more literate syntax
    also removed debugger comments.

--- features/step_definitions/web_screen_steps.rb
+++ features/step_definitions/web_screen_steps.rb
@@ -10,22 +10,20 @@
 
 Then /^(?:|I )should see "([^\"]*)" on the screen(?: within "([^\"]*)")?$/ do |text, selector|
   with_scope(selector) do
-    # debugger
-    assert page.find(:xpath, "//*[contains(text(), '#{text}')]").visible?
+    dom_node = page.find(:xpath, "//*[contains(text(), '#{text}')]")
+    dom_node.should be_visible
   end
 end
 
 Then /^(?:|I )should not see "([^\"]*)" on the screen(?: within "([^\"]*)")?$/ do |text, selector|
   with_scope(selector) do
-    # debugger
-    assert !page.find(:xpath, "//*[contains(text(), '#{text}')]").visible?
+    dom_node = page.find(:xpath, "//*[contains(text(), '#{text}')]")
+    dom_node.should_not be_visible
   end
 end
 
-Then /^(?:|I )should see "([^\"]*)" on the screen$/ do |text|
-  if defined?(Spec::Rails::Matchers)
-    page.find('.expandable .body').visible?
-  else
-    assert page.find('.expandable .body').visible?
+When /^I click on "([^\"]*)" of the (\d+)(?:st|nd|rd|th) post$/ do |text,pos|
+  within("#posts div:nth-child(#{pos.to_i})") do
+    page.find(:xpath, "//*[contains(text(), '#{text}')]").click
   end
 end
