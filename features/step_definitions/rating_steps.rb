b944c19f79a8fd0562e812d323e9eee7f61b6f1f
Stephen Bannasch <stephen.bannasch@gmail.com>
2010-03-22 21:28:30 -0400

    strip spaces from text content containing rating

--- features/step_definitions/rating_steps.rb
+++ features/step_definitions/rating_steps.rb
@@ -1,6 +1,6 @@
 Then /^I should see a rating of (\d+) on the (\d+)(?:st|nd|rd|th) post$/ do |rating, pos|
   within("#posts div:nth-child(#{pos.to_i})") do
-    page.find(:css, "dl.rating dt").text.should == rating
+    page.find(:css, "dl.rating dt").text.strip.should == rating
   end
 end
 
